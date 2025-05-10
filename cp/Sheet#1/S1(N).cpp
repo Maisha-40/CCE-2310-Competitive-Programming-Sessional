0ee54c344356f98bfbeabcc43ccf1b2119333c7e
Sadia Humaira Maisha <sadiahumairamaisha@gmail.com>
2025-05-10 19:53:05 +0600

    Update S1(N).cpp

--- cp/Sheet#1/S1(N).cpp
+++ cp/Sheet#1/S1(N).cpp
@@ -1,16 +1,20 @@
+1.// Problem name: N. Char
+2.//Course title: Competitive Programming Sessional 
+3.//Course code: 2310
+4.// Course teacher name: Mirza Raquib
+5.//Date: 23/4/25
 #include<bits/stdc++.h>
 using namespace std;
-
 int main()
 {
     char X;
-    cin >> X;
-
-    if (islower(X)) {
-        cout << char(X - 32) << endl;
+    cin>>X;
+    if(islower(X)) {
+    cout<<char(X - 32)<<endl;
     } else if (isupper(X)) {
-        cout << char(X + 32) << endl;
-    } else {
-        cout << "Invalid input. Please enter an alphabet letter." << endl;
+    cout<<char(X + 32)<<endl;
+    } 
+    else {
+    cout<< "Invalid input.enter an alphabet letter." <<endl;
     }
 }

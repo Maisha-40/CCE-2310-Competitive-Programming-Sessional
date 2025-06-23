4452128fbc09e29a7d1edc123b8f5faa7f383f6d
Sadia Humaira Maisha <sadiahumairamaisha@gmail.com>
2025-06-23 22:25:32 +0600

    Add files via upload

---
+++ Sheet#4/S4(B).cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string s,t;
+   getline(cin,s);
+   for(int i=0;i<s.size();i++)
+   {
+       if(s[i]=='\\')
+       {
+           break;
+       }
+       else
+        cout<<s[i];
+   }
+
+}
